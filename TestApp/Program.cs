66c36a3671cea35ecffa27484d63ad26c60952c5
Aleksa <aleksavasiljevic07@gmail.com>
2019-04-11 11:59:16 +0200

    dsd

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("234123");
+            Console.WriteLine("234123"); 
         }
     }
 }
